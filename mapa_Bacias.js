1129532722ccfa656a1131c01009dbfd83d05a01
GU <gusera4325@gmail.com>
2024-04-26 15:59:00 -0300

    a

--- mapa_Bacias.js
+++ mapa_Bacias.js
@@ -1,5 +1,5 @@
-
-var cardAcre = document.getElementById('Card_Acre')
+/*Card do Acre */
+var cardAcre = document.getElementById('card_acre')
 
 function mapBacias_Coloca (){
 
@@ -23,6 +23,34 @@ function mapBacias_tira(){
     cardAcre.style.display = 'none'
     
     interuptor = true
+
+
+}
+
+/*Card do Amazonas */
+var cardAmazonas = document.getElementById('card_amazonas')
+
+function mapBacias1_Coloca (){
+
+
+
+
+
+cardAmazonas.style.height = '150px'
+cardAmazonas.style.display = 'block'
+
+
+
+
+}
+
+function mapBacias1_tira(){
+
+
+
+
+    cardAmazonas.style.display = 'none'
+    
 
 
 }
