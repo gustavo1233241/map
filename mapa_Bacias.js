3dd2281357276184ae978be01dd2c4ecd6b5adc2
gu <gusera4325@gmail.com>
2024-04-29 15:48:46 -0300

    a

--- mapa_Bacias.js
+++ mapa_Bacias.js
@@ -1,56 +1,172 @@
 /*Card do Acre */
 var cardAcre = document.getElementById('card_acre')
 
-function mapBacias_Coloca (){
+function mapBacias_ColocaAcre() {
 
-
-
-
-
-cardAcre.style.height = '150px'
-cardAcre.style.display = 'block'
-
-
-
+    cardAcre.style.height = '150px'
+    cardAcre.style.display = 'block'
 
 }
 
-function mapBacias_tira(){
-
-
-
+function mapBacias_tiraAcre() {
 
     cardAcre.style.display = 'none'
-    
-    interuptor = true
-
 
 }
+
+
+
+/*Card de Alto Tapajós*/
+var cardAlto = document.getElementById('card_altotapajós')
+
+function mapBacias_ColocaAltotapajós() {
+    cardAlto.style.height = '150px'
+    cardAlto.style.display = 'block'
+}
+function mapBacias_tiraAltotapajós() {
+    cardAlto.style.display = 'none'
+}
+
+
+
+/*Card de Almada */
+var cardAlmada = document.getElementById('card_almada')
+
+function mapBacias_ColocaAlmada() {
+    cardAlmada.style.height = '150px'
+    cardAlmada.style.display = 'block'
+}
+function mapBacias_tiraAlmada() {
+    cardAlmada.style.display = 'none'
+}
+
+
 
 /*Card do Amazonas */
 var cardAmazonas = document.getElementById('card_amazonas')
 
-function mapBacias1_Coloca (){
-
-
-
-
-
-cardAmazonas.style.height = '150px'
-cardAmazonas.style.display = 'block'
-
-
-
-
+function mapBacias_ColocaAmazonas() {
+    cardAmazonas.style.height = '150px'
+    cardAmazonas.style.display = 'block'
 }
-
-function mapBacias1_tira(){
-
-
-
-
+function mapBacias_tiraAmozonas() {
     cardAmazonas.style.display = 'none'
-    
-
-
 }
+
+
+/*Card do Araripe */
+var cardAraripe = document.getElementById('card_araripe')
+
+function mapBacias_ColocaAraripe() {
+    cardAraripe.style.height = '150px'
+    cardAraripe.style.display = 'block'
+}
+function mapBacias_tiraAraripe() {
+    cardAraripe.style.display = 'none'
+}
+
+/*Card do Bananal */
+var cardBananal = document.getElementById('card_bananal')
+
+function mapBacias_ColocaBananal() {
+    cardBananal.style.height = '150px'
+    cardBananal.style.display = 'block'
+}
+function mapBacias_tiraBananal() {
+    cardBananal.style.display = 'none'
+}
+
+
+/*Card de Barreirinhas_terra */
+var cardBarreirinhas = document.getElementById('card_barreirinhas_terra')
+
+function mapBacias_ColocaBarreirinhas() {
+    cardBarreirinhas.style.height = '150px'
+    cardBarreirinhas.style.display = 'block'
+}
+function mapBacias_tiraBarreirinhas() {
+    cardBarreirinhas.style.display = 'none'
+}
+
+/*Card de Bragança*/
+var cardBraganca = document.getElementById('card_bragança_Vizeu')
+
+function mapBacias_ColocaBragança() {
+    cardBraganca.style.height = '150px'
+    cardBraganca.style.display = 'block'
+}
+function mapBacias_tiraBragança() {
+    cardBraganca.style.display = 'none'
+}
+
+
+/*Card de Camamu*/
+var cardCamamu = document.getElementById('card_camamu')
+
+function mapBacias_ColocaCamamu() {
+    cardCamamu.style.height = '150px'
+    cardCamamu.style.display = 'block'
+}
+function mapBacias_tiraCamamu() {
+    cardCamamu.style.display = 'none'
+}
+
+
+/*Card de Campos*/
+var cardCampos = document.getElementById('card_campos')
+
+function mapBacias_ColocaCampos() {
+    cardCampos.style.height = '150px'
+    cardCampos.style.display = 'block'
+}
+function mapBacias_tiraCampos() {
+    cardCampos.style.display = 'none'
+}
+
+
+
+/*Card de Cumuruxatiba*/
+var cardCumuruxatiba = document.getElementById('card_cumuruxatiba')
+
+function mapBacias_ColocaCumuruxatiba() {
+    cardCumuruxatiba.style.height = '150px'
+    cardCumuruxatiba.style.display = 'block'
+}
+function mapBacias_tiraCumuruxatiba() {
+    cardCumuruxatiba.style.display = 'none'
+}
+
+/*Card de Ceará*/
+var cardCeará = document.getElementById('card_ceará')
+
+function mapBacias_ColocaCeará() {
+    cardCeará.style.height = '150px'
+    cardCeará.style.display = 'block'
+}
+function mapBacias_tiraCeará() {
+    cardCeará.style.display = 'none'
+}
+
+/*Card de Espírito Santo*/
+var cardEspíritoSanto = document.getElementById('card_espírito')
+
+function mapBacias_ColocaEspíritoSanto() {
+    cardEspíritoSanto.style.height = '150px'
+    cardEspíritoSanto.style.display = 'block'
+}
+function mapBacias_tiraEspíritoSanto() {
+    cardEspíritoSanto.style.display = 'none'
+}
+
+/*Card de mapBacias_Coloca Foz do Amazonas*/
+var cardEspíritoSanto = document.getElementById('card_espírito')
+
+function mapBacias_ColocaEspíritoSanto() {
+    cardEspíritoSanto.style.height = '150px'
+    cardEspíritoSanto.style.display = 'block'
+}
+function mapBacias_tiraEspíritoSanto() {
+    cardEspíritoSanto.style.display = 'none'
+}
+
+
